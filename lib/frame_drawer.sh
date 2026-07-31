92ac9ded86c1228077e338dfd63b5456253af784
MavikBow <mrmavikd@gmail.com>
2026-07-31 18:15:53 +0300

    improved frame I/O operations

--- lib/frame_drawer.sh
+++ lib/frame_drawer.sh
@@ -25,27 +25,27 @@ function setup_empty_frame {
 	local y="$YELLOW"
 	local n="$NOCOLOR"
 	
-	echo "" >> $1
-	echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}" >> $1
-	echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}" >> $1
-	echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}" >> $1
-	echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}" >> $1
-	echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}" >> $1
-	echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}" >> $1
-	echo "" >> $1
-	echo " ${b}q${n} ${b}w${n} ${b}e${n} ${b}r${n} ${b}t${n} ${b}y${n} ${b}u${n} ${b}i${n} ${b}o${n} ${b}p${n}" >> $1
-	echo "  ${b}a${n} ${b}s${n} ${b}d${n} ${b}f${n} ${b}g${n} ${b}h${n} ${b}j${n} ${b}k${n} ${b}l${n}" >> $1 
-	echo "   ${b}z${n} ${b}x${n} ${b}c${n} ${b}v${n} ${b}b${n} ${b}n${n} ${b}m${n}" >> $1
-	echo "\n" >> $1
-	echo "" >> $1
+	{
+		echo ""
+		echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}"
+		echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}"
+		echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}"
+		echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}"
+		echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}"
+		echo "      ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n} ${b}_${n}"
+		echo ""
+		echo " ${b}q${n} ${b}w${n} ${b}e${n} ${b}r${n} ${b}t${n} ${b}y${n} ${b}u${n} ${b}i${n} ${b}o${n} ${b}p${n}"
+		echo "  ${b}a${n} ${b}s${n} ${b}d${n} ${b}f${n} ${b}g${n} ${b}h${n} ${b}j${n} ${b}k${n} ${b}l${n}"
+		echo "   ${b}z${n} ${b}x${n} ${b}c${n} ${b}v${n} ${b}b${n} ${b}n${n} ${b}m${n}"
+		echo "\n"
+		echo ""
+	} > "$1"
 }
 
 function draw_frame {
 	echo -ne "\033[1A"
 	echo -ne "\r\033[K"
 	echo -ne "\033[11A"
-	echo -ne "$(cat $1)"
-	echo -ne "\033[${LINES}D"
-	echo -ne "\033[1B"
-	echo -ne "\033[${LINES}D"
+	echo -ne "$(cat "$1")"
+	echo -ne "\r\033[1B\r"
 }
